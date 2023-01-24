59bb5f4b6945d07b1650e9c61910c65b47b9dcc3
Scott <scotthenry1@gmail.com>
2023-01-23 23:29:06 -0500

    Completed lab submission

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left - 1}px`;
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left + 1}px`;
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+  });
+
